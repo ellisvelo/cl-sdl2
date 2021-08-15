2172b8cf8fff6f856f2b54316428318663e0fa55
Shawn Ellis <shawn.ellis17@gmail.com>
2021-08-15 18:39:31 -0500

    Updated specs with SDL-2.0.16 and LLVM-11.1.0

--- src/autowrap.lisp
+++ src/autowrap.lisp
@@ -18,7 +18,7 @@
  :sysincludes `,(cl:append
                  #+openbsd (cl:list "/usr/X11R6/include")
                  #+(and unix (not darwin))
-                 (cl:list "/usr/lib/clang/8.0.1/include/"))
+                 (cl:list "/usr/lib/clang/11.1.0/include/" "/usr/include/"))
  :exclude-definitions ("SDL_main"
                        "SDL_LogMessageV"
                        "SDL_vsnprintf"
